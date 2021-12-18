185f4c0ccb2ec2e6f1e10b2df892b03f59d27760
alondgr <alon.sela87@gmail.com>
2021-12-18 10:50:25 +0200

    pl

--- src/config/routes.js
+++ src/config/routes.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 const usersController = require('../controllers/users.controllers');
 const postsController = require('../controllers/posts.controller');
-// const upload = multer({ dest: 'public/' })
 const jwt = require('jsonwebtoken')
 const multer = require('multer')
 const storage = multer.diskStorage({
